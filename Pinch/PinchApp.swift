d7b6d26bea07aba829970ba0579d6549062fe456
juanchete <juanlopezlmg@gmail.com>
2024-02-22 21:43:30 -0400

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Juan Lopez on 22/2/24.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
