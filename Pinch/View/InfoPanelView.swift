857497a3db156a95107f4ad04593c0faad89fb31
juanchete <juanlopezlmg@gmail.com>
2024-02-23 21:43:39 -0400

    feat: Long Press Gesture

---
+++ Pinch/View/InfoPanelView.swift
@@ -0,0 +1,18 @@
+//
+//  InfoPanelView.swift
+//  Pinch
+//
+//  Created by Juan Lopez on 23/2/24.
+//
+
+import SwiftUI
+
+struct InfoPanelView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InfoPanelView()
+}
