b07cb540ce44aec268184918f43d811c94821d27
juanchete <juanlopezlmg@gmail.com>
2024-02-23 22:09:35 -0400

    Control Interface

---
+++ Pinch/View/ControlImageView.swift
@@ -0,0 +1,18 @@
+//
+//  ControlImageView.swift
+//  Pinch
+//
+//  Created by Juan Lopez on 23/2/24.
+//
+
+import SwiftUI
+
+struct ControlImageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ControlImageView()
+}
